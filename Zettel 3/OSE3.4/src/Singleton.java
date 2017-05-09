6a6480edc59e8c2da492234096d07e88a5376fb3
Keridos <fuffzehn@gmail.com>
2017-05-09 14:00:20 +0200

    Z3 aufgabe 4.

---
+++ Zettel 3/OSE3.4/src/Singleton.java
@@ -0,0 +1,16 @@
+/**
+ * Created by nico on 09/05/17.
+ */
+public class Singleton {
+    private static Singleton instance;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
